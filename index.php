561e8b7b21635725a73947f21078bd5d2cd9e8ea
ballboy <ballboyst365@gmail.com>
2025-01-02 11:13:33 +0900

    時刻表示コード

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>タイトル</title>
+        <meta name="description" content="width=device-width,inital-scale=1, shrink-to-fit=no">
+<!-- Bootstrap CSS -->
+<link rel='stylesheet' here='css/style.css'>
+
+    </head>
+    <body>
+	<header>
+        <h1 class='font-weight-normal'>よくわかるPHPの教科書</h1>
+	</header>
+    <main>
+	<h2>Practice</h2>
+	<pre>
+	<?php
+	/* ここにPHPのプログラムを記述する */
+	date_default_timezone_set('Asia/Tokyo');
+	print('現在は' .date('G時 i分 s秒').'です');
+	?>
+	</pre>
+	</main>
+    </body>
+</html>
+
